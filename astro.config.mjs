20edf0f2b0708240b3ba011c2e67d70bb2f98f8a
Vingdy <34713921+Vingdy@users.noreply.github.com>
2024-09-14 22:57:43 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Vingdy.github.io",
+  site: "https://vingdy.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
